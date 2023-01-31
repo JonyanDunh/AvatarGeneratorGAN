421d8096c6f22340921f061e033b667ed1c46b17
JONYANDUNH <jonyandunh@outlook.com>
2023-02-01 01:28:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def image2tensor():
 images = torch.load(r"/mnt/d/data/saved_tensor/gan_images/ganyu-images_128.pt").permute(3, 0, 1, 2).to(
     dtype=torch.float32, device="cuda") / 255
 
-
+print(images.shape[2])
 class View(nn.Module):
     def __init__(self, shape):
         super().__init__()
@@ -51,20 +51,15 @@ class Discriminator_Model(nn.Module):
             nn.Conv2d(128, 128, kernel_size=16, stride=2), nn.BatchNorm2d(128), nn.LeakyReLU(0.2),
             nn.Conv2d(128, 128, kernel_size=16, stride=2), nn.BatchNorm2d(128), nn.LeakyReLU(0.2),
             nn.Conv2d(128, 3, kernel_size=16, stride=2), nn.LeakyReLU(0.2),
-            View(3 * 18 * 18),
-            nn.Linear(3 * 18 * 18, 1),
-            # nn.Sigmoid()
         )
         self.model128 = nn.Sequential(
             nn.Conv2d(3, 256, kernel_size=8, stride=2), nn.BatchNorm2d(256), nn.LeakyReLU(0.2),
             nn.Conv2d(256, 256, kernel_size=8, stride=2), nn.BatchNorm2d(256), nn.LeakyReLU(0.2),
             nn.Conv2d(256, 3, kernel_size=8, stride=2), nn.LeakyReLU(0.2),
-            # View(3 * 10 * 10),
-            # nn.Linear(3 * 10 * 10, 1),
-            # nn.Sigmoid()
         )
 
         self.fc1 = nn.Linear(3 * 10 * 10, 1)
+        self.fc2 = nn.Linear(3 * 18 * 18, 1)
         self.optimizer = optim.SGD(self.parameters(), lr=0.0005)
         self.loss_fn = nn.BCEWithLogitsLoss().cuda()
         self.loss_train = 0.0
@@ -102,15 +97,11 @@ class Generator_Model(nn.Module):
         super().__init__()
 
         self.model512 = nn.Sequential(
-            nn.Linear(100, 3 * 19 * 19),
-            nn.LeakyReLU(0.2),
-            View((1, 3, 19, 19)),
             nn.ConvTranspose2d(3, 128, kernel_size=16, stride=2), nn.BatchNorm2d(128), nn.LeakyReLU(0.2),
             nn.ConvTranspose2d(128, 128, kernel_size=16, stride=2), nn.BatchNorm2d(128), nn.LeakyReLU(0.2),
             nn.ConvTranspose2d(128, 128, kernel_size=16, stride=2), nn.BatchNorm2d(128), nn.LeakyReLU(0.2),
             nn.ConvTranspose2d(128, 3, kernel_size=16, stride=2, padding=1), nn.BatchNorm2d(3), nn.Sigmoid(),
 
-            View((1, 3, 512, 512))
         )
         self.model128 = nn.Sequential(
             nn.ConvTranspose2d(3, 256, kernel_size=8, stride=2), nn.BatchNorm2d(256), nn.LeakyReLU(0.2),
@@ -118,6 +109,7 @@ class Generator_Model(nn.Module):
             nn.ConvTranspose2d(256, 3, kernel_size=8, stride=2, padding=1), nn.BatchNorm2d(3), nn.Sigmoid(),
         )
         self.fc1 = nn.Linear(100, 3 * 11 * 11)
+        self.fc2 = nn.Linear(100, 3 * 19 * 19)
         self.optimizer = optim.AdamW(self.parameters(), lr=0.08)
         self.loss_train = 0.0
         self.items = 0
@@ -148,10 +140,9 @@ class Generator_Model(nn.Module):
         self.loss_train += loss.item()
         writer.add_scalar('Generator Training loss/times', loss.item(), self.counter)
         if self.items == 0:
-
             writer.add_image('Images-Epoch-{}'.format(self.Epoch), Generator_outputs[0])
             self.Epoch += 1
-        if self.counter % 200 == 0:
+        if self.counter % 1000 == 0:
             # plt.imshow(
             #     ((Generator_outputs[0].detach()) * 255).reshape(3, 128, 128).permute(1, 2, 0).to(dtype=torch.int).cpu())
             # plt.show()
@@ -187,23 +178,8 @@ def training_loop():
         if Epoch % 50 == 0:
             global t
             torch.save(Discriminator.state_dict(), '/mnt/d/data/saved_model/gan_images/gan_images-Discriminator-{}.pt'.format(int(t)))
-            torch.save(Generator.state_dict(), '/mnt/d/data/saved_model/gan_images/gan_images-Generator-{}.pt'.format(int(t)))
+            torch.save(Generator.state_dict(), '/mnt/d/data/saved_model/gan_images/gan_images-Generator-{}-{}.pt'.format(images.shape[2],int(t)))
 
 
-# def validate(model):
-#     correct = 0
-#     total = 0
-#     with torch.no_grad():
-#         for imags, labels in torch.utils.data.DataLoader(
-#                 [(fake_images, torch.tensor([1], dtype=torch.float32, device="cuda")) for fake_images in
-#                  images], batch_size=1024, shuffle=False):
-#             outputs = model(imags)
-#             print(outputs)
-#             total += imags.shape[0]
-#             correct += float(outputs.sum())
-#     print("Accuracy : {:.4f}".format(correct / total))
-
-
-# validate(distinguish_model)
 
 training_loop()
